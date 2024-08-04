6df590d3547c35c3b13f2ba8dbb6ad3fc0d6cd98
mubbe127 <mubarek.tajudin@gmail.com>
2024-08-05 00:56:49 +0200

    finished main functions in gameboard class and added player class

--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
 export { ship };
 
 class ship {
-    
-    constructor(length) {
-    this.hit = 0
-    this.length=length
-    }
-  
-    getLength() {
-      return this.length;
-    }
+  constructor(length) {
+    this.hit = 0;
+    this.length = length;
+  }
 
-    getHitCount() {
-      return this.hit;
-    }
-    incrementHit() {
-      if (this.hit >= this.length) {
-        return;
-      }
-      this.hit++;
-      return this.hit;
-    }
+  getLength() {
+    return this.length;
+  }
 
-    isSunk() {
-      if (this.length === this.hit) {
-        return true;
-      } else {
-        return false;
-      }
+  getHitCount() {
+    return this.hit;
+  }
+  incrementHit() {
+    if (this.hit >= this.length) {
+      return;
     }
-  
+    this.hit++;
+    return this.hit;
+  }
+
+  isSunk() {
+    if (this.length === this.hit) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 function cell(i, j) {
+  this.ship = null;
+  this.miss = false;
+  this.beenHit = false;
   this.value = [i, j];
-  this.link = [];
 }
 
 class gameBoard {
   constructor() {
     this.board = this.createArray();
-    this.queue = [];
+    this.ships = [];
   }
 
   createArray() {
@@ -54,33 +54,6 @@ class gameBoard {
       }
     }
     return array;
-  }
-
-  placeShip(length, [x, y], [z, w]) {
-    if (x !== z && y !== w) {
-      console.log("jiep");
-      return;
-    }
-    const newShip = ship(length);
-    if (y === w && z >= x) {
-      this.board[x][y] = newShip;
-      while (z >= x) {
-        this.board[x++][y] = newShip;
-      }
-    }
-    if (x === z && w >= y) {
-      this.board[x][y] = newShip;
-      while (w >= y) {
-        this.board[x][y++] = newShip;
-        console.log(y);
-      }
-    }
-    if (y === w && x >= z) {
-      this.board[x][y] = newShip;
-      while (x >= z) {
-        this.board[x--][y] = newShip;
-      }
-    }
   }
 
   isWithinBounds(x, y) {
@@ -97,102 +70,101 @@ class gameBoard {
     let array = [];
     moves.forEach(([nx, ny]) => {
       if (this.isWithinBounds(nx, ny)) {
-       
-
         array.push([nx, ny]);
       } else array.push(0);
     });
 
     let finalarray = [];
 
-    console.log(array)
+    console.log(array);
 
     array.forEach((item, index) => {
       let collided = false;
       if (!(item instanceof Array)) {
+        finalarray.push(item);
         return false;
       }
-      let [a,b] = [x,y]
-      console.log(x,y)
-      console.log(a,b)
+      let [a, b] = [x, y];
+
       const [nx, ny] = item;
-      if (this.board[a][b] instanceof ship) {
-        console.log([a,b])
-         console.log("this can jiep")
+      if (this.board[x][y].ship instanceof ship) {
+        finalarray.push(0);
         return false;
       }
+
       if (index === 0) {
         while (nx > a) {
-          if (this.board[a++][b] instanceof ship) {
-            console.log(nx)
-            console.log("index 0")
-            console.log(a)
+          a++;
+          if (this.board[a][b].ship instanceof ship) {
             collided = true;
           }
         }
         if (collided === false) {
           finalarray.push(item);
         } else {
-            console.log(a)
-            finalarray.push(0);}
-            collided=false
+          finalarray.push(0);
+        }
+        collided = false;
       }
       if (index === 1) {
         while (a > nx) {
-          if (this.board[a--][b] instanceof ship) {
+          a--;
+          if (this.board[a][b].ship instanceof ship) {
             collided = true;
-            console.log("index 1")
-            
+            console.log("index 1");
           }
         }
         if (collided === false) {
           finalarray.push(item);
         } else {
-            finalarray.push(0)
-            collided=false}
+          finalarray.push(0);
+          collided = false;
+        }
       }
       if (index === 2) {
         while (ny > b) {
-          if (this.board[a][b++] instanceof ship) {
+          b++;
+          if (this.board[a][b].ship instanceof ship) {
             collided = true;
-            console.log("index 2")
-            
+            console.log("index 2");
           }
         }
         if (collided === false) {
           finalarray.push(item);
         } else {
-            finalarray.push(0)
-            collided=false}; 
+          finalarray.push(0);
+          collided = false;
+        }
       }
       if (index === 3) {
-        while (a > ny) {
-          if (this.board[a][b--] instanceof ship) {
-            console.log("index 3")
+        while (b >= ny) {
+          b--;
+          if (this.board[a][b] instanceof ship) {
+            console.log("index 3");
             collided = true;
-            
           }
         }
         if (collided === false) {
           finalarray.push(item);
         } else {
-            collided=false
-            finalarray.push(0)};
+          collided = false;
+          finalarray.push(0);
+        }
       }
     });
-    return finalarray
+    return finalarray;
   }
 
   placeTheShip(x, y, length, move) {
     let array = this.battlemove(x, y, length);
 
-    console.log(array[0])
+    console.log(array);
 
     switch (move) {
-      case "up":
+      case "down":
         array = array[0];
         break;
-      case "down":
+      case "up":
         array = array[1];
         break;
       case "left":
@@ -205,36 +177,79 @@ class gameBoard {
       default:
         console.log("jiep");
     }
-    const [z, w] = array;
-    if (x !== z && y !== w) {
-      console.log("jiep");
+    if (array instanceof Array) {
+      const [z, w] = array;
+      if (x !== z && y !== w) {
+        console.log("jiep");
+        return;
+      }
+      const newShip = new ship(length);
+      this.ships.push(newShip);
+      if (y === w && z >= x) {
+        this.board[x][y].ship = newShip;
+
+        while (z >= x) {
+          this.board[x++][y].ship = newShip;
+        }
+      }
+      if (x === z && w >= y) {
+        this.board[x][y].ship = newShip;
+        while (w >= y) {
+          this.board[x][y++].ship = newShip;
+          console.log(y);
+        }
+      }
+      if (y === w && x >= z) {
+        this.board[x][y].ship = newShip;
+        while (x >= z) {
+          this.board[x--][y].ship = newShip;
+        }
+      }
+    } else {
+      console.log("No path found");
+    }
+  }
+  recieveAttack([x, y]) {
+    if (
+      this.board[x][y].ship instanceof ship &&
+      this.board[x][y].beenHit === false
+    ) {
+      this.board[x][y].beenHit = true;
+      this.board[x][y].ship.incrementHit();
+    } else if (this.board[x][y].beenHit === true) {
       return;
+    } else {
+      this.board[x][y].miss = true;
     }
-    const newShip = new ship(length);
-    if (y === w && z >= x) {
-      this.board[x][y] = newShip;
-      while (z >= x) {
-        this.board[x++][y] = newShip;
-      }
-    }
-    if (x === z && w >= y) {
-      this.board[x][y] = newShip;
-      while (w >= y) {
-        this.board[x][y++] = newShip;
-        console.log(y);
-      }
-    }
-    if (y === w && x >= z) {
-      this.board[x][y] = newShip;
-      while (x >= z) {
-        this.board[x--][y] = newShip;
-      }
-    }
+  }
+
+  allShipSunk() {
+
+    return this.ships.every(item => 
+        item.isSunk()
+
+    )
   }
 }
 
-const thegame = new gameBoard();
-thegame.placeTheShip(6,0,2, "down") 
-thegame.placeTheShip(4,0,2, "down") 
-console.log(thegame.board[5][0])
+Player {
 
+    constructor(name){
+
+        this.name= 
+        this.gameBoard= new gameBoard()
+    }
+}
+
+const thegame = new gameBoard();
+thegame.placeTheShip(6, 0, 2, "down");
+thegame.placeTheShip(4, 0, 2, "up");
+thegame.recieveAttack([4, 0]);
+thegame.recieveAttack([3, 0]);
+thegame.recieveAttack([7,0])
+thegame.recieveAttack([6,0])
+console.log(thegame.board[3][0]);
+console.log(thegame.board[4][0]);
+console.log(thegame.board[7][0]);
+console.log(thegame.ships[1].isSunk())
+console.log(thegame.allShipSunk())
